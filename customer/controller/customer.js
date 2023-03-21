3e13f9eb55ed5c73746e0e3701ff7dbcd174ba27
seun awonugba <86803598+seunAwonugba@users.noreply.github.com>
2023-03-21 23:29:37 +0100

    implement find customer by email

--- customer/controller/customer.js
+++ customer/controller/customer.js
@@ -2,8 +2,9 @@ const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const { BadRequest } = require("../errors");
 const { customer, address } = require("../models");
 
-const findCustomerById = async (req, res, next) => {
+exports.findCustomerById = async (req, res, next) => {
     const { id } = req.params;
+    // console.log(req.params);
 
     try {
         const findCustomerById = await customer.findByPk(id, {
@@ -14,7 +15,7 @@ const findCustomerById = async (req, res, next) => {
             ],
         });
 
-        if (!customer) {
+        if (!findCustomerById) {
             return next(new BadRequest("Customer does not exist"));
         }
 
@@ -30,4 +31,37 @@ const findCustomerById = async (req, res, next) => {
     }
 };
 
-module.exports = { findCustomerById };
+exports.findCustomerByEmail = async (req, res, next) => {
+    const { email } = req.params;
+    // console.log(`Email requested -> ${email}`);
+
+    try {
+        const findCustomerByEmail = await customer.findOne({
+            where: { email },
+            include: [
+                {
+                    model: address,
+                },
+            ],
+        });
+
+        // console.log(findCustomerByEmail);
+
+        if (!findCustomerByEmail) {
+            return next(
+                new BadRequest("Customer with this email does not exist")
+            );
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            data: findCustomerByEmail,
+        });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            data: ReasonPhrases.INTERNAL_SERVER_ERROR,
+        });
+    }
+};
+
+// module.exports = { findCustomerById, findCustomerByEmail };
--- customer/router/customer.js
+++ customer/router/customer.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { findCustomerById } = require("../controller/customer");
+const {
+    findCustomerById,
+    findCustomerByEmail,
+} = require("../controller/customer");
 const customerRouter = express.Router();
 
 customerRouter.get("/:id", findCustomerById);
+customerRouter.get("/email/:email", findCustomerByEmail);
 
 module.exports = { customerRouter };
